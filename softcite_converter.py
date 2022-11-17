79930a6461956dd52f91c9f9fbd318fd235f0b61
Esteban Gonzalez Guardia <egonzalez@ciclope.info>
2022-11-17 06:09:26 +0100

    Procedure to calculate positions changed

--- softcite_converter.py
+++ softcite_converter.py
@@ -5,9 +5,13 @@ tei_doc = "/Users/estebangonzalezguardia/projects/software_benchmark/softcite_co
 #CLEANR = re.compile('<.*?>') 
 
 def write_brat_file(name, content, annotations):
-    f = open("/Users/estebangonzalezguardia/projects/software_benchmark/somesci_files/"+str(name)+".ann","w")
+    f = open("somesci_files/"+str(name)+".ann","w")
     count = 1
+    cursor_content = 0
+    content_text = content
     for annotation in annotations:
+        print("**************")
+        print(content_text)
         id = "T"+str(count)
         type = ""
         if annotation.get("type") == "software":
@@ -18,16 +22,24 @@ def write_brat_file(name, content, annotations):
             type = "Developer"
         elif annotation.get("type") == "url":
             type = "URL"
-        start = content.index(annotation.get_text())
-        end = start + len(annotation)
+        print(annotation)
         text = annotation.get_text()
-
+        print(text)
+        start = cursor_content + content_text.index(text)
+        end = start + len(text)
+        
         f.write(str(id)+"    "+str(type)+" "+str(start)+" "+str(end)+" "+str(text)+"\n")
         count = count + 1
+
+        cursor_content = end
+
+        content_text = content_text[(content_text.index(text)+len(text)):]
+        
+
     f.close()
 
 def write_txt_file(name, content):
-    f = open("/Users/estebangonzalezguardia/projects/software_benchmark/somesci_files/"+str(name)+".txt","w")
+    f = open("somesci_files/"+str(name)+".txt","w")
     f.write(content)
     f.close
 
@@ -43,6 +55,7 @@ with open(tei_doc, 'r') as tei:
         print("Analizing text....")
         article_text = article.find('text')
         annotations = article_text.find_all(lambda tag: tag.name=="rs" and (tag.get("type")=="software" or tag.get("type")=="version" or tag.get("type")=="publisher" or tag.get("type")=="url"))
+        print("Annotations:"+str(annotations))
         print("Creating txt file ...")
         write_txt_file(pmc.get_text(),text.get_text())
         print("Creating BRAT file ....")
